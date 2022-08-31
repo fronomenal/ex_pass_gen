c33185fb7aaacbe6ea4899065c765a35bb2cb6f2
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-08-31 20:00:14 +0100

    Initialized project with failing tests

---
+++ test/passgen_test.exs
@@ -0,0 +1,61 @@
+defmodule PassgenTest do
+  use ExUnit.Case
+  doctest Passgen
+
+  setup do
+    options = %{"len" => "8", "nums" => "false", "caps" => "false", "syms" => "false"}
+
+    opts_type = %{
+      lows: Enum.map(?a..?z, &<<&1>>),
+      nums: Enum.map(0..9, &Integer.to_string(&1)),
+      upps: Enum.map(?A..?Z, &<<&1>>),
+      syms: String.split("!#$%&()%+,-./:;<=>?@[]^_{|}~", "", trim: true)
+    }
+
+    {:ok, res} = PassGen.gen(options)
+
+    %{opts_type: opts_type, res: res}
+
+  end
+
+  test "returns a string", %{res: res} do
+    assert is_bitstring(res)
+  end
+  test "returns an error when no length is given" do
+    options = %{"invalid" => "true"}
+
+    assert {:err, _} = PassGen.gen(options)
+  end
+  test "returns an error when length is not an integer" do
+    options = %{"len" => "*"}
+
+    assert {:err, _} = PassGen.gen(options)
+  end
+  test "length of returned string is the provided option" do
+    options = %{"len" => "6"}
+
+    {:ok, res} = PassGen.gen(options)
+
+    assert options["len"] == String.length(res)
+  end
+  test "returns only lowercase string as default", %{opt_type: opts} do
+    options = %{"len" => "6"}
+
+    {:ok, res} = PassGen.gen(options)
+
+    assert !String.contains?(res, opts.upps)
+    refute String.contains?(res, opts.nums)
+    refute String.contains?(res, opts.syms)
+  end
+  test "Returns error when all options except len are not bools" do
+    options = %{"len" => "6", "syms" => "invalid"}
+
+    assert {:err, _} = PassGen.gen(options)
+  end
+  test "Returns error for invalid options" do
+    options = %{"len" => "6", "invalid" => "true"}
+
+    assert {:err, _} = PassGen.gen(options)
+  end
+
+end
